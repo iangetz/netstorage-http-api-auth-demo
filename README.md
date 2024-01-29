7f301b65584f5d0b57f1dbd841f81976cfa822c6
Ian Getz <iangetz@tumana.com>
2024-01-29 19:01:26 +0000

    Initial commit of working demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# NetStorage HTTP API Auth Demo
+
+Sample node.js code to leverage the Akamai NetStorage HTTP API and list contents in a NetStorage storage group. Anyone has permission to copy this code, modify it, and incorporate it into their project.
+
+Copy or rename the file in /server/creds to 'netstorage.json' and populate the fields with your storage group and credential information. Note: The uploadDir value must have a starting and trailing slash.
---
+++ server/index.js
@@ -0,0 +1,49 @@
+const netstorage = require('./creds/netstorage.json');
+const crypto = require('crypto');
+const https = require('https');
+
+// Adjust the action value (e.g. list, dir, stat, etc) as needed
+const acsAction = 'version=1&action=list';
+
+// Build and sign auth headers
+let acsAuthData = '', acsAuthSign = '';
+try {
+	acsAuthData = `5, 0.0.0.0, 0.0.0.0, ${Math.floor(Date.now() / 1000)}, ${Math.floor((Math.random() * 100000))}, ${netstorage.uploadId}`;
+	const sign_string = `${netstorage.uploadDir}\nx-akamai-acs-action:${acsAction}\n`;
+	const message = acsAuthData + sign_string;
+	acsAuthSign = crypto.createHmac('sha256', netstorage.uploadKey)
+		.update(message)
+		.digest('base64');
+} catch (err) {
+	throw new Error(`[Auth Error] ${err}`);
+}
+
+// HTTPS request settings
+const options = {
+	hostname: `${netstorage.domainPrefix}${netstorage.apiDomain}`,
+	port: 443,
+	path: `${netstorage.uploadDir}`,
+	method: 'GET',
+	headers: {
+		"Accept": "text/html",
+		'X-Akamai-ACS-Action': acsAction,
+		'X-Akamai-ACS-Auth-Data': acsAuthData,
+		'X-Akamai-ACS-Auth-Sign': acsAuthSign,
+		'Host': `${netstorage.domainPrefix}${netstorage.apiDomain}`
+	}
+};
+
+const req = https.request(options, (res) => {
+	console.log('statusCode:', res.statusCode);
+	console.log('headers:', res.headers);
+
+	res.on('data', (data) => {
+		process.stdout.write(data);
+	});
+});
+
+req.on('error', (e) => {
+	console.error(e);
+});
+
+req.end();
